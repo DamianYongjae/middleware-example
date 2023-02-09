4906514db8d108cf514a97a448f94f38e50c4ad5
DamianYongjae <klick214@gmail.com>
2023-02-09 11:38:16 -0500

    update

--- pages/testmiddleware.tsx
+++ pages/testmiddleware.tsx
@@ -1,7 +1,7 @@
 const TestMiddleWare = () => {
   return (
     <div className="fixed top-0 left-0 h-screen w-screen bg-white text-black z-99999">
-      test middleware
+      this is test middleware
     </div>
   );
 };
